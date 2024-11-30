4edbb77d5c47dcf2a0bdf724ddb2a9729a6bc4f5
SdAnubis <120252809+OsoPanda1@users.noreply.github.com>
2024-11-30 13:26:31 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+// Opcional: Funcionalidad adicional para los botones
+document.querySelector('.bg-blue-800').addEventListener('click', () => {
+    alert('Iniciar sesión');
+});
+
+document.querySelector('.border-blue-800').addEventListener('click', () => {
+    alert('Registro');
+});
